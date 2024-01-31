9af428a3672d3d1a1d93d6eb035b58cbf037070f
brkuvezdic <branimirkuvezdic@gmail.com>
2024-01-31 19:09:57 +0100

    Add avatar field to user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,6 +5,10 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    avatar: {
+      type: String,
+      default: "https://cdn.jsdelivr.net/npm/react-icons/fa/FaUser.js",
+    },
   },
   { timestamps: true }
 );
