e74cfa2b2004f4bd4aa87894f4dfd0bfd75467a0
brkuvezdic <branimirkuvezdic@gmail.com>
2024-02-06 18:47:07 +0100

    First app test for deploying

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -20,7 +21,7 @@ const app = express();
 app.listen(8888, () => {
   console.log("Server radi na portu 8888!");
 });
-
+const __dirname = path.resolve();
 app.use(express.json());
 
 app.use(cookieParser());
@@ -30,6 +31,11 @@ app.use("/api/auth", authRouter);
 
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Server error";
