60c2ca0dd2e8d9eb8711751bf999de98080c55ce
brkuvezdic <branimirkuvezdic@gmail.com>
2024-02-06 18:39:20 +0100

    Event delete function with api + tested in insomnia

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -8,3 +8,18 @@ export const createListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(403, "Forbidden - not your event"));
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Event deleted");
+  } catch (error) {}
+
+  next(error);
+};
