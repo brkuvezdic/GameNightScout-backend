df3a045e87623999b3a2d0e669e8dd0d22ddc52b
brkuvezdic <branimirkuvezdic@gmail.com>
2024-02-06 19:33:04 +0100

    API route for event editing + only update works in Insomnia but cant display error???

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,3 +1,4 @@
+import { errorHandler } from "../utils/error.js";
 import Listing from "../models/listing.model.js";
 
 export const createListing = async (req, res, next) => {
@@ -22,4 +23,25 @@ export const deleteListing = async (req, res, next) => {
   } catch (error) {}
 
   next(error);
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Event not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "That is not your event!!"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
 };
