ae62d5153031a9f0817bcc69067fa717f3be9ac2
brkuvezdic <branimirkuvezdic@gmail.com>
2024-02-08 22:37:54 +0100

    BETA VERZIJA FILTRIRANJA PT1

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -57,3 +57,41 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+
+    const filter = {
+      $or: [
+        { name: { $regex: searchTerm, $options: "i" } },
+        { description: { $regex: searchTerm, $options: "i" } },
+        { city: { $regex: searchTerm, $options: "i" } },
+        { genre: { $regex: searchTerm, $options: "i" } },
+      ],
+    };
+
+    if (req.query.ageOver18) {
+      filter.ageOver18 = req.query.ageOver18 === "true";
+    }
+
+    if (req.query.slot) {
+      filter.slot = parseInt(req.query.slot);
+    }
+
+
+    const listings = await Listing.find(filter)
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
