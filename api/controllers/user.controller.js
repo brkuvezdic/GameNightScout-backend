0579778e4c7af66f79ef07f4f57a930db20f9180
brkuvezdic <branimirkuvezdic@gmail.com>
2024-02-10 13:23:33 +0100

    Backend for displaying which user created the event

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -46,7 +46,7 @@ export const getUserListings = async (req, res, next) => {
       next(error);
     }
   } else {
-    return next(errorHandler(401, 'You can only view your own listings!'));
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
 
@@ -61,6 +61,20 @@ export const deleteUser = async (req, res, next) => {
       .status(200)
       .json("Account has been deleted...")
       .clearCookie("access_token");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) return next(errorHandler(404, "User not found!"));
+
+    const { password: pass, ...rest } = user._doc;
+
+    res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
